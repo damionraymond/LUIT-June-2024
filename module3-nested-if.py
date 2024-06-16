a9cda6501e1e9d08b53b0e3f1d82c70ca8457350
Damion Raymond <dkraym01+github@gmail.com>
2024-06-16 12:45:07 -0500

    Create module3-nested-if.py

---
+++ module3-nested-if.py
@@ -0,0 +1,8 @@
+answer_a = input('Do you like traveling? y/n: ')
+if answer_a == 'y':
+    answer_b = input('Do you like Asia? y/n: ')
+    if answer_b == 'y':
+        print('Excellent! You can win a ticket to Thailand!')
+else:
+    print('Sorry to hear that!')
+
