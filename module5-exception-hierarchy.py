dcc5bf55efdb8d606fac46a23f8ef41f1ad1998c
Damion Raymond <dkraym01+github@gmail.com>
2024-06-23 17:36:58 -0500

    Create module5-exception-hierarchy.py

---
+++ module5-exception-hierarchy.py
@@ -0,0 +1,25 @@
+# import sys
+
+# user_name = input('What is your name?')
+# if user_name == '':
+#    print('Empty name? I cannot work with that. I am closing the program. Bye!')
+#    sys.exit()
+# print('Hello', user_name)
+# print('Let us get started...') 
+
+# while True:
+#     print('hi!')
+
+# programing_languages = ['Java','Python','C++']
+# print(programing_languages[10])
+
+# #Key Error
+# ages = {'Jim': 30, 'Pam': 28, 'Kevin': 31}
+# ages['Michael']
+
+#Type Error
+# age = input('What is your age?')
+# print('In 10 years, you will be', age + 10)
+
+#Value Error
+age =int(input('What is your age? '))
